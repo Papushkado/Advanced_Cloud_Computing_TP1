8a8a11a5e7108441d0be6fada9905a4c15b5be29
Papushkado <stephen.cohen.pro@gmail.com>
2024-09-16 22:26:47 +0200

    set_up file in bash to set up instances faster

---
+++ set_up.sh
@@ -0,0 +1,11 @@
+sudo apt-get update && sudo apt-get upgrade -y
+
+sudo apt-get install python3 python3-pip -y
+
+sudo apt install python3-fastapi -y
+
+sudo apt install python3-uvicorn -y
+
+sudo apt install uvicorn -y
+
+uvicorn main:app --host 0.0.0.0 --port 8000
