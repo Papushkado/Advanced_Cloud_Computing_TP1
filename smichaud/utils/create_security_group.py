52fd6d01c81d0f9f7e723acc575d846b67aee159
smichaud-dbox <smichaud@d-box.com>
2024-09-17 17:17:53 -0400

    add creation of security group

---
+++ smichaud/utils/create_security_group.py
@@ -0,0 +1,34 @@
+# Need to allow ssh to all machines
+import boto3
+import os
+
+
+def create_security_group(ec2_client, group_name, group_description):
+    # Create a security group
+    response = ec2_client.create_security_group(
+        GroupName=group_name,
+        Description=group_description
+    )
+
+    group_id = response['GroupId']
+
+    #todo verify this
+    ec2_client.authorize_security_group_ingress(
+            GroupId=group_id,
+            IpPermissions=[
+                {
+                    'IpProtocol': 'tcp',
+                    'FromPort': 22,
+                    'ToPort': 22,
+                    'IpRanges': [{'CidrIp': '0.0.0.0/0'}]  # Allow SSH
+                },
+                {
+                    'IpProtocol': 'tcp',
+                    'FromPort': 8000,
+                    'ToPort': 8000,
+                    'IpRanges': [{'CidrIp': '0.0.0.0/0'}]  # Allow FastAPI
+                }
+            ]
+        )
+
+    return group_id
