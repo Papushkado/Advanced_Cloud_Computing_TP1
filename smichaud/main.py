d9c3564ff2c3c77c8918028e405fb53b68bd6f60
Zabiullah-sz <zabiullah.shair-zaie@polymtl.ca>
2024-09-29 19:04:04 -0400

    benchmarking.py

--- smichaud/main.py
+++ smichaud/main.py
@@ -1,6 +1,8 @@
+import asyncio
 import boto3
 import os
 import time
+from utils.benchmarking import get_public_ip, run_benchmark
 from utils.ec2_instances_launcher import launch_ec2_instance, shutdown_running_instances
 from utils.create_key_pair import generate_key_pair
 from utils.create_security_group import create_security_group
@@ -10,17 +12,21 @@ from dotenv import load_dotenv
 
 from instances_ressources.workers.bootstrap import get_user_data
 from instances_ressources.load_balancer.bootstrap import get_lb_user_data
+import logging
+import sys
+import os
 
-# Retrieve AWS credentials from .env file
 os.environ.pop('AWS_ACCESS_KEY_ID', None)
 os.environ.pop('AWS_SECRET_ACCESS_KEY', None)
 os.environ.pop('AWS_SESSION_TOKEN', None)
 # Load .env file
 load_dotenv(dotenv_path='./.env')
 
+# Retrieve AWS credentials from .env file
 aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
 aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
 aws_session_token = os.getenv('AWS_SESSION_TOKEN')
+print(aws_access_key_id)
 
 key_pair_name = 'log8415E-tp1-key-pair'
 # Create EC2 client
@@ -45,17 +51,17 @@ private_instance_cluster0 = launch_ec2_instance(
     public_ip=True, # for now because when private lb cant communicate to it. Not sure how to fix. Look at it last
     user_data = worker_user_data, 
     tag=("CLUSTER", "0"), 
-    num_instances=1) #TODO PUT CORRECT NUMBER OF INSTANCES
+    num_instances=4) #TODO PUT CORRECT NUMBER OF INSTANCES
 
 private_instance_cluster1 = launch_ec2_instance(
     ec2, 
     key_pair_name, 
     group_id, 
-    "t2.micro",  #TODO PUT CORRECT TYPE OF INSTANCE
+    "t2.large",  #TODO PUT CORRECT TYPE OF INSTANCE
     public_ip=True, # for now because when private lb cant communicate to it. Look at it last
     user_data = worker_user_data, 
     tag=("CLUSTER", "1"),
-    num_instances=1) #TODO PUT CORRECT NUMBER OF INSTANCES
+    num_instances=4) #TODO PUT CORRECT NUMBER OF INSTANCES
 
 
 lb_user_data = get_lb_user_data(aws_access_key_id, aws_secret_access_key, aws_session_token)
@@ -68,16 +74,23 @@ lb_instance = launch_ec2_instance(
     user_data = lb_user_data,
     num_instances=1)
 
-lb_instance_id = lb_instance[0][0]
-time.sleep(300)
+lb_instance_id = "i-028eaef00ac705b21"
+time.sleep(500)
 
 #TODO:
 #get public ip adress from lb instance id
+
+# Retrieve load balancer public IP and run the benchmark
+lb_public_ip = get_public_ip(ec2, lb_instance_id)
+print(f"Load Balancer Public IP: {lb_public_ip}")
+
+# Run the benchmark
+asyncio.run(run_benchmark(lb_public_ip))
+
 #run benchmark and send command to /cluster1 and /cluster2
 #   get back results needed in the report. Check what are those in the énoncé
 # 
 # cleanup instances, security group, keypair 
-
 
 ##### In this part, we are going to clean_up, all the set up environnement
 
