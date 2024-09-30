f0d9f2f079ce1b4278f6758da29b0b9ce6f4805b
Papushkado <stephen.cohen.pro@gmail.com>
2024-09-30 18:03:08 +0200

    clean code clean_up

--- main.py
+++ main.py
@@ -108,8 +108,10 @@ def clean_up(ec2, instance_ids, key_name, group_id):
     delete_security_group(ec2, group_id) # We need all the instances to be deleted before deleting the security group
     
 print("\n Cleaning up instances and security group ...")
-instance_ids = [private_instance_cluster0[0][0], private_instance_cluster1[0][0], lb_instance_id]
+
+instance_ids = [lb_instance_id]
 for p in range(len(private_instance_cluster0)):
     instance_ids.append(private_instance_cluster0[p][0])
     instance_ids.append(private_instance_cluster1[p][0])
+    
 clean_up(ec2, instance_ids, key_pair_name, group_id)
